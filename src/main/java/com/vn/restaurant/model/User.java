1b92d0386c43f032ece2bfaa15dcab915b367fbb
NhatThang19 <122526522+NhatThang19@users.noreply.github.com>
2026-04-04 16:49:50 +0700

    first commit

---
+++ src/main/java/com/vn/restaurant/model/User.java
@@ -0,0 +1,31 @@
+package com.vn.restaurant.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    private String fullName;
+
+    private Boolean enabled = true;
+}
